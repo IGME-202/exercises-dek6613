48da385c82cc5cdd63ace62568fd32f592a29b72
dek6613 <dek6613@rit.edu>
2020-09-06 17:28:58 -0400

    Completed exercise 2
    Added snow particles, tree and snowman prefabs, and a script that places 2 trees onto the map at fixed locations

---
+++ Snowman Demo/Assets/Scripts/SpawnTrees.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrees : MonoBehaviour
+{
+    public GameObject treePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(treePrefab, new Vector3(45, 7, 55), Quaternion.identity);
+        Instantiate(treePrefab, new Vector3(49, 7, 30), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
